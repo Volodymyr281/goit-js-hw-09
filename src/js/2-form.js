e09f819c7f914e7283fe71fd257911016517cae9
Volodymyr <klavavova2001@gmail.com>
2025-11-23 19:11:04 +0200

    initial commit

---
+++ src/js/2-form.js
@@ -0,0 +1,43 @@
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+    email: '',
+    message: '',
+    };
+
+    const form = document.querySelector('.feedback-form');
+
+    const savedData = localStorage.getItem(STORAGE_KEY);
+
+    if (savedData) {
+    formData = JSON.parse(savedData);
+
+    form.elements.email.value = formData.email || '';
+    form.elements.message.value = formData.message || '';
+    }
+
+    form.addEventListener('input', e => {
+    const { name, value } = e.target;
+
+    formData[name] = value.trim();
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    });
+
+    form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    if (!formData.email || !formData.message) {
+        alert('Fill please all fields');
+        return;
+    }
+
+    console.log(formData);
+
+    localStorage.removeItem(STORAGE_KEY);
+    formData = { email: '', message: '' };
+    form.reset();
+    });
+
+
+
